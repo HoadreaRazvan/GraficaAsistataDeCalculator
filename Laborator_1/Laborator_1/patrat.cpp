26173ef7f9a2406686842fa5f4f58da2cab8d42d
HoadreaRazvan <162281824+HoadreaRazvan@users.noreply.github.com>
2024-03-08 21:42:51 +0200

    Desenare Cerc

--- Laborator_1/Laborator_1/patrat.cpp
+++ Laborator_1/Laborator_1/patrat.cpp
@@ -1,18 +1,27 @@
 ﻿/*
 Programul afiseaza un patrat pe care il translateaza pe axa x la apasarea sagetilor stanga, dreapta
 */
+#include <cmath>
+
 #include "glos.h"
 
 #include "gl.h"
 #include "glu.h"
 #include "glaux.h"
 
-static GLfloat x = 0;
+static GLfloat x = 0,y=0,r=0;
 
 void myInit() {
     glClearColor(1.0, 1.0, 1.0, 1.0);
 }
 
+void CALLBACK rot_z_up(AUX_EVENTREC* event) {
+    r += 15;
+}
+
+void CALLBACK rot_z_down(AUX_EVENTREC* event) {
+    r -= 15;
+}
 void CALLBACK MutaStanga()
 {
     x = x - 10;
@@ -23,25 +32,45 @@ void CALLBACK MutaDreapta()
     x = x + 10;
 }
 
+void CALLBACK MutaSus()
+{
+    y = y + 10;
+}
+
+void CALLBACK MutaJos()
+{
+    y = y - 10;
+}
+
 void CALLBACK display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glLoadIdentity();
 
-    glTranslatef(x, 0, 0.0);
+    glTranslatef(x, y, 0.0);
 
-    glBegin(GL_QUADS);
+   glRotatef(r, 0, 0, 1);
+
+    //glBegin(GL_QUADS);
+    //{
+    //    glColor3f(1.0, 0.0, 0.0);
+    //    glVertex2f(100, 100);
+    //    glColor3f(1.0, 1.0, 0.0);
+    //    glVertex2f(150.0, 100.0);
+    //    glColor3f(0.0, 0.0, 1.0);
+    //    glVertex2f(150.0, 150.0);
+    //    glColor3f(0.0, 1.0, 0.0);
+    //    glVertex2f(100.0, 150.0);
+    //}
+
+    glBegin(GL_POLYGON);
     {
         glColor3f(1.0, 0.0, 0.0);
-        glVertex2f(100, 100);
-        glColor3f(1.0, 1.0, 0.0);
-        glVertex2f(150.0, 100.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex2f(150.0, 150.0);
-        glColor3f(0.0, 1.0, 0.0);
-        glVertex2f(100.0, 150.0);
+        for (double i = 0; i <= 360; i += 1)
+            glVertex2f(50 * cos(i * 3.1415 / 180), 50 * sin(i * 3.1415 / 180));
     }
+
     glEnd();
 
     glFlush();
@@ -80,13 +109,17 @@ void CALLBACK myReshape(GLsizei w, GLsizei h)
 int main(int argc, char** argv)
 {
     auxInitDisplayMode(AUX_SINGLE | AUX_RGB);
-    auxInitPosition(0, 0, 800, 600);
+    auxInitPosition(500, 300, 1500, 1000);
     auxInitWindow("Un patrat");
     myInit();
     auxKeyFunc(AUX_LEFT, MutaStanga);
     auxKeyFunc(AUX_RIGHT, MutaDreapta);
 
-    //desenam un cerc
+    auxKeyFunc(AUX_UP, MutaSus);
+    auxKeyFunc(AUX_DOWN, MutaJos);
+
+    auxMouseFunc(AUX_LEFTBUTTON, AUX_MOUSEDOWN, rot_z_up);
+    auxMouseFunc(AUX_RIGHTBUTTON, AUX_MOUSEDOWN, rot_z_down);
 
     auxReshapeFunc(myReshape);
     auxMainLoop(display);
